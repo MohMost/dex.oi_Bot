4ec57cda20963bcc36131341ef32c0a573d2daf3
unknown <teemohmost2020@gmail.com>
2023-02-09 23:12:06 +0100

    :bug: (fix): directory bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
 import Canvas from "canvas";
 import * as discord from "discord.js";
-import { main } from "./commands/commandBuilder.js";
-import { typeMap } from "./data/icons.js";
-import { typeWS, typeWS2 } from "./data/stren_weak.js";
+import { main } from "../commands/commandBuilder.js";
+import { typeMap } from "../data/icons.js";
+import { typeWS, typeWS2 } from "../data/stren_weak.js";
 import { InteractionResponseType } from "discord.js";
 
 config();
